04f7bd2711c4e5551ee53c08af5a9a644fa5b035
Eric Lazalde <eclazalde@gmail.com>
2015-12-10 16:10:30 -0700

    lab 4 finished

--- lab4/ModEx.py
+++ lab4/ModEx.py
@@ -1,19 +1,48 @@
-import binascii
-
-from OpenSSL import rand
+import random
+import OpenSSL
+import itertools
 
 class ModEx(object):
-	p = 0;
-	#def __init__(self):
-	#	p = 0
 	
 	def run(self):
-		p = rand.bytes(500)
-		print binascii.hexlify(p)
-		print p.decode('ascii')
-		#for character in p:
-		 # print character.encode('hex')
+		#g= 5
+		g = 2573041524068575501546774721958551382793444058852959448173073424708835049483214746377114621027868168763968221918355434732930964212304265577079830246362
+		s = 0
+		p = 0
+		f  = open('large numbers.txt', 'r')
+
+		for line in f:
+			l = line.split('=')
+			if l[0] == 's':
+				s = int(l[1])
+			if l[0] == 'p':
+				p = int(l[1])
 		
+		if p == 1:
+			print 0
+			
+		result = 1
+		g = g % p
+		while s > 0:
+			if (s % 2 == 1):
+				result = (result * g) % p
+			s = s >> 1
+			g = (g * g) % p
+		
+		print result
+		
+		
+class LargeRandom(object):
+	
+	def run(self):
+		#
+		# Generate large number for s
+		p = 0
+		p = random.SystemRandom(random.seed()).getrandbits(500)
+		print "{}".format(p)
+		
+			
 
 if __name__ == '__main__':
+	#LargeRandom().run()
     ModEx().run()
